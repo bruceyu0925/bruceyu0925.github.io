afc5b91aa3c6830c651560094e8c6beb35b198c4
bruceyu0925 <bruceyu0925@gmail.com>
2022-03-24 18:00:04 +0800

    調整poster為分批請求，code待優化

--- js/graphic_design.js
+++ js/graphic_design.js
@@ -88,14 +88,8 @@
 
     // Switch --------------------------------------------------
 
-    // const
-    const SwitchBtn = queAll( '.switch-btn' );
-
-    // event 點選切換
-    SwitchBtn.forEach( el => {
-
+    queAll( '.switch-btn' ).forEach( el => {
         el.onclick = () => {
-
             el.parentElement.classList.toggle( '--after' );
             el.parentElement.classList.toggle( '--before' );
         }
@@ -104,18 +98,12 @@
     // Blog --------------------------------------------------
 
     // const 搜尋
-    const BlogAi       = getId( 'BlogAi' ),
-          BlogPs       = getId( 'BlogPs' ),
-          BlogSort     = getId( 'BlogSort' ),
-          BlogSortIcon = queAll( '.blog-find-sort-state' ),
-          BlogText     = getId( 'BlogText' ),
-          BlogClear    = getId( 'BlogClear' ),
-          BlogSearch   = getId( 'BlogSearch' );
-
-    // const 狀態
-    const BlogPrint    = getId( 'BlogPrint' ),
-          BlogBigpic   = getId( 'BlogBigpic' ),
-          BlogList     = getId( 'BlogList' );
+    const BlogBlock  = getId( 'BlogBlock' ),
+          BlogOption = getId( 'BlogOption' ),
+          BlogText   = getId( 'BlogText' ),
+          BlogClear  = getId( 'BlogClear' ),
+          BlogSearch = getId( 'BlogSearch' ),
+          BlogPrint  = getId( 'BlogPrint' );
 
     // const 清單
     const BlogLs       = getId( 'BlogLs' ),
@@ -136,131 +124,218 @@
           BlogDesc     = getId( 'BlogDesc' ),
           BlogImg      = getId( 'BlogImg' );
 
-    // var 搜尋功能
-    var Blog_Ai,
-        Blog_Ps,
+    // var 資料陣列
+    var Blog_Kind  = [],
+        Blog_Array = [],
+        Blog_Order = 0,
+        Blog_Total = 0,
+        Blog_Count = 0;
+
+    // var 資料請求
+    var Blog_Page = 0,
         Blog_Sort,
+        Blog_Tool = [],
         Blog_Text;
 
-    // var 搜尋結果
-    var Blog_Array = [],
-        Blog_Order,
-        Blog_Total,
-        Blog_Count;
+    // func GET類別
+    fetch( GAS( 'AKfycbycpeSbczbsH2gNn9PYSXI8C8NoIPXCOK9hTHCPh6HdL9UM_oPgnBEbRqpCKtqDPfJk' ) , {
+        method: 'GET'
 
-    // func Ajax請求
-    function BlogData() {
+    }).then( res => {
+        return res.json()
+        
+    }).then( data => {
 
-        fetch( GAS( 'AKfycbxwwozBbZ4CYTLcPWSQlEsvB1EDmCtj8Wr9KnVnuCEELt4AC7f1mmuqSDNM0Dh2E2It' ) , {
-            method: 'POST',
-            headers: { 'Content-Type' : 'application/x-www-form-urlencoded; charset=utf-8' },
-            body: JSON.stringify({
-                'ai'   : Blog_Ps,
-                'ps'   : Blog_Ai,
-                'sort' : Blog_Sort,
-                'text' : Blog_Text
-            })
+        Blog_Kind = data;
 
+        // 新增Dom
+        for( let i = 0 ; i < data.length ; i++ ) {
+            getId( 'BlogKind' ).insertAdjacentHTML( 'beforeend' ,
+                `<li class="blog-kind-li">
+                    <input type="checkbox" class="blog-kind-check" value="${ data[ i ][ 'Id' ] }" id="K-${ data[ i ][ 'Kind' ] }">
+                    <label for="K-${ data[ i ][ 'Kind' ] }">${ data[ i ][ 'Kind' ] }</label>
+                </li>`
+            )
+        }
+
+        // 新增事件
+        queAll( '.blog-kind-check' ).forEach( el => {
+            el.onclick = () => el.toggleAttribute( 'checked' )
+        })
+
+        BlogSearch.click()
+    })
+
+    // func GET資料
+    function BlogData( page , tool , text , sort ) {
+
+        fetch( GAS( 'AKfycbwi9ZeGuMtffjXfcSHKdjPV0pard7uGyYkbHbFRRluxKmQD9Ii3K6YtumsAl0CrlEWh2g' ) + 
+            `?len=12&page=${ page }&tool=${ tool }&text=${ text }&sort=${ sort }` , {
+            method: 'GET'
+    
         }).then( res => {
             return res.json()
 
         }).then( data => {
-            Blog_Array = data;
-            Blog_Total = data.length;
-            BlogHtml();
+            Blog_Total = data.len;
+            BlogHtml( data.data );
         })
     }
 
     // func 輸出html
-    function BlogHtml() {
+    function BlogHtml( data ) {
 
-        Blog_Count = queAll( '.blog-li' ).length;
+        data.forEach( n => {
 
-        for ( let i = 0 ; i < 12 ; i++ ) {
-
-            if ( Blog_Count < Blog_Total ) {
-
-                var n     = Blog_Array[ Blog_Count ],
-                    num   = n[ 'Num' ],
-                    name  = n[ 'Name' ],
-                    date  = n[ 'Date' ],
-                    tool  = n[ 'Tool' ],
-                    src   = n[ 'Src' ],
-                    style = n[ 'Style' ];
-                            
-                var m = 
-                    `<div class="blog-li-cover __tran200ms">
-                        <span class="blog-li-span __tran200ms">${ name }</span>
-                    </div>`;
-
-                if ( DeviceJudge() === true ) m = '';
-
-                BlogLs.insertAdjacentHTML( 'beforeend' ,
-                    `<li class="blog-li" value="${ Blog_Count }">
-                        <span class="blog-li-num">${ num }</span>
-                        <div class="blog-li-block">
-                            ${ m }
-                            <img class="blog-li-img __tran200ms __imgresp"
-                                alt="${ name }" style="${ style }" src="${ src }" width="349" height="494">
+            // 類別
+            Blog_Kind.forEach( el =>
+                n.Kind = n.Kind.replace( el.Id.toString() , el.Kind )
+            );
+            n.Kind = n.Kind.replace( ',' , ' , ' );
+            
+            // 新增Dom
+            BlogLs.insertAdjacentHTML( 'beforeend' ,
+                `<li class="blog-li" data-order="${ Blog_Count }" data-id="${ n.Id }">
+                    <span class="blog-li-num">${ n.Num }</span>
+                    <div class="blog-li-block">
+                        <div class="blog-li-cover __tran200ms">
+                            <span class="blog-li-span __tran200ms">${ n.Title }</span>
                         </div>
-                        <span class="blog-li-name">${ name }</span>
-                        <span class="blog-li-tool">${ tool }</span>
-                        <span class="blog-li-date">${ date }</span>
-                    </li>`
-                );
-                Blog_Count++;
+                        <img class="blog-li-img __tran200ms" alt="${ n.Title }"
+                            style="top:${ n.Top };left:${ n.Left };" src="${ n.Src }">
+                    </div>
+                    <span class="blog-li-name">${ n.Title }</span>
+                    <span class="blog-li-tool">${ n.Kind }</span>
+                    <span class="blog-li-date">${ dateTran( n.DateUpdate ) }</span>
+                </li>`
+            );
+            Blog_Array.push( n );
+            Blog_Count++;
+        });
+
+        // 設定圖片
+        queAll( '.blog-li-img' ).forEach( el => {
+            if( el.offsetWidth > el.offsetHeight ) {
+                el.style.width  = 'auto';
+                el.style.height = '100%';
+            } else {
+                el.style.width  = '100%';
+                el.style.height = 'auto';
             }
-        }
+        });
+
+        // 新增事件
         queAll( '.blog-li' ).forEach( el => 
             el.addEventListener( 'click' , BlogClick )    
         );
 
-        BlogShow();
+        // 讀取完畢
+        switch( true ) {
+
+            case Blog_Total > Blog_Count:
+                BlogAppend.classList.remove( '--hide' );
+                break;
+
+            case Blog_Total <= Blog_Count:
+                BlogAppend.classList.add( '--hide' );
+                break;
+        }
+
+        switch( true ) {
+
+            case Blog_Total > 0:
+                BlogPrint.innerHTML = `${ Blog_Total }筆相符結果`;
+                BlogWarn.classList.remove( '--show' );
+                break;
+
+            case Blog_Total === 0:
+                BlogPrint.innerHTML = `未搜尋到相關資料`;
+                BlogWarn.classList.add( '--show' );
+                break;
+        }
+
+        BlogLoad.classList.remove( '--show' );
+        BlogSearch.disabled = false;
     };
 
-    // func 顯示html
-    function BlogShow() {
-
-        setTimeout( () => {
-
-            BlogLoad.classList.remove( '--show' );
-
-            queAll( '.blog-li' ).forEach( el => el.classList.add( '--show' ) );
-            
-            BlogJudge();
-
-            BlogSearch.disabled = false;
-
-        } , 1000 )
+    // event 搜尋功能 - 工具
+    BlogOption.onclick = () => {
+        BlogBlock.classList.toggle( '--show' )
     }
 
-    // func 判斷資料
-    function BlogJudge() {
-
-        // 判斷資料數量
-        if ( Blog_Total === 0 ) {
-            BlogPrint.innerHTML = `未搜尋到相關資料`;
-            BlogWarn.classList.add( '--show' );
-
-        } else {
-            BlogPrint.innerHTML = `${ Blog_Total }筆相符結果`;
-            BlogWarn.classList.remove( '--show' );
+    // event 搜尋功能 - 排序
+    queAll( '.blog-sort-li' ).forEach( el => {
+        el.onclick = () => {
+            queOne( '.blog-sort-li.--click' ).classList.remove( '--click' );
+            el.classList.add( '--click' );
         }
+    })
 
-        // 判斷是否還有資料
-        if ( Blog_Total <= Blog_Count ) {
-            BlogAppend.classList.add( '--hide' )
+    // event 搜尋功能 - 文字
+    BlogText.onkeyup = function() {
+        this.value === '' ?
+            BlogClear.classList.add( '--hide' ):
+            BlogClear.classList.remove( '--hide' );
+     }
 
-        } else {
-            BlogAppend.classList.remove( '--hide' )
-        }
+    // event 搜尋功能 - 清空
+    BlogClear.onclick = function() {
+        this.classList.add( '--hide' );
+        BlogText.value = '';
     }
+
+    // event 搜尋功能 - 搜尋
+    BlogSearch.onclick = () => {
+        
+        // 讀取畫面
+        BlogSearch.disabled = true;
+        BlogAppend.classList.add( '--hide' );
+        BlogLoad  .classList.add( '--show' );
+        BlogWarn  .classList.remove( '--show' );
+        BlogBlock .classList.remove( '--show' );
+        BlogLs   .innerHTML = '';
+        BlogPrint.innerHTML = '搜尋中...';
+
+        // 搜尋條件
+        Blog_Count = 0;
+        Blog_Page  = 0;
+        Blog_Array = [];
+        Blog_Tool  = [];
+        queAll( '.blog-kind-check' ).forEach( el => {
+            el.hasAttribute( 'checked' ) ? Blog_Tool.push( el.value ) : null ;
+        });
+        Blog_Tool.join( ',' );
+        Blog_Text = BlogText.value.toString().toLowerCase();
+        Blog_Sort = queOne( '.blog-sort-li.--click' ).value;
+        BlogData( Blog_Page , Blog_Tool , Blog_Text , Blog_Sort );
+    }
+
+    // event 顯示方式
+    queAll( '.blog-find-array-btn' ).forEach( el => {
+        el.onclick = () => {
+            var cl = queOne( '.blog-find-array-btn.--click' );
+            BlogLs.classList.remove( cl.value );
+            BlogLs.classList.add( el.value );
+            cl.classList.remove( '--click' );
+            el.classList.add( '--click' );
+            BlogLs.scroll( 0 , 0 )
+        }
+    })
+
+    // event 加載按鈕
+    BlogAppend.onclick = () => {
+        Blog_Page++;
+        BlogAppend.classList.add( '--hide' );
+        BlogLoad  .classList.add( '--show' );
+        BlogData( Blog_Page , Blog_Tool , Blog_Text , Blog_Sort );
+    }
+
 
     // func 開啟全屏
     function BlogClick() {
         BlogFull.classList.add( '--show' );
         Html    .classList.add( '--lock' );
-        Blog_Order = this.getAttribute( 'value' );
+        Blog_Order = this.getAttribute( 'data-order' );
         BlogOpen();
     }
 
@@ -269,23 +344,14 @@
 
         BlogAside.scroll( 0 , 0 );
 
-        var n = Blog_Array[ Blog_Order ],
-            num  = n[ 'Num' ],
-            name = n[ 'Name' ],
-            date = n[ 'Date' ],
-            tool = n[ 'Tool' ],
-            desc = n[ 'Desc' ],
-            src  = n[ 'Src' ],
-            alt  = n[ 'Name' ];
-
-        BlogNum .innerHTML = num;
-        BlogName.innerHTML = name;
-        BlogDate.innerHTML = date;
-        BlogTool.innerHTML = tool;
-        BlogDesc.innerHTML = desc;
-        
-        BlogImg.setAttribute( 'src' , src );
-        BlogImg.setAttribute( 'alt' , alt );
+        var n = Blog_Array[ Blog_Order ];
+        BlogNum .innerHTML = n.Num;
+        BlogName.innerHTML = n.Title;
+        BlogDate.innerHTML = dateTran( n.DateUpdate );
+        BlogTool.innerHTML = n.Kind;
+        BlogDesc.innerHTML = n.Desc;
+        BlogImg.setAttribute( 'src' , n.Src );
+        BlogImg.setAttribute( 'alt' , n.Title );
 
         switch( true ) {
 
@@ -311,102 +377,6 @@
         }
     }
 
-    // event 搜尋功能 - Illustrator
-    BlogAi.onclick = function () {
-
-        this.classList.toggle( '--on' );
-
-        if ( this.classList.contains( '--on' ) ) {
-            this.value = 'Illustrator'
-
-        } else {
-            this.value = '-'
-        }
-    }
-
-    // event 搜尋功能 - Photoshop
-    BlogPs.onclick = function () {
-
-        this.classList.toggle( '--on' );
-
-        if ( this.classList.contains( '--on' ) ) {
-            this.value = 'Photoshop'
-
-        } else {
-            this.value = '-'
-        }
-    }
-
-    // event 搜尋功能 - sort
-    BlogSort.onclick = () => {
-
-        BlogSortIcon.forEach( el => el.classList.toggle( '--show' ) );
-
-        var v = queOne( '.blog-find-sort-state.--show' ).getAttribute( 'data-blog-sort' );
-
-        BlogSort.value = v;
-    }
-
-    // event 搜尋功能 - input
-    BlogText.onkeyup = () => {
-
-        if ( BlogText.value === '' ) {
-            BlogClear.classList.remove( '--show' )
-
-        } else {
-            BlogClear.classList.add( '--show' )
-        }
-    }
-
-    // event 搜尋功能 - clear
-    BlogClear.onclick = () => {
-        BlogText.value = '';
-        BlogClear.classList.remove( '--show' );
-    }
-
-    // event 搜尋功能 - search
-    BlogSearch.onclick = () => {
-
-        BlogSearch.disabled = true;
-
-        Blog_Ps   = BlogPs  .value;
-        Blog_Ai   = BlogAi  .value;
-        Blog_Sort = BlogSort.value;
-        Blog_Text = BlogText.value.toLowerCase();
-
-        BlogAppend.classList.add( '--hide' );
-        BlogLoad  .classList.add( '--show' );
-        BlogWarn  .classList.remove( '--show' );
-        BlogLs    .innerHTML = '';
-        BlogPrint .innerHTML = '搜尋中...';
-
-        BlogData();
-    }
-
-    // event 顯示方式 - big picture
-    BlogBigpic.onclick = () => {
-        BlogBigpic.classList.add( '--click' );
-        BlogList  .classList.remove( '--click' );
-        BlogLs    .classList.add( '--bigpic' );
-        BlogLs    .classList.remove( '--list' );
-    }
-
-    // event 顯示方式 - list
-    BlogList.onclick = () => {
-        BlogBigpic.classList.remove( '--click' );
-        BlogList  .classList.add( '--click' );
-        BlogLs    .classList.remove( '--bigpic' );
-        BlogLs    .classList.add( '--list' );
-        BlogLs    .scroll( 0 , 0 );
-    }
-
-    // event 加載按鈕
-    BlogAppend.onclick = () => {
-        BlogAppend.classList.add( '--hide' );
-        BlogLoad  .classList.add( '--show' );
-        BlogHtml();
-    }
-
     // event 全屏 prev
     BlogPrev.onclick = () => {
         Blog_Order--;
@@ -424,8 +394,5 @@
         BlogFull.classList.remove( '--show' );
         Html    .classList.remove( '--lock' );
     }
-
-    // 搜尋
-    BlogSearch.click();
 
 }()
